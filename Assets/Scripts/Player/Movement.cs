6cb72c78de5cf454a953c6d49b4a581cefe10751
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2020-10-03 16:35:03 +0300

    Main Hero full move & idle anim.
     - Modified move script
     - Added move & idle anim.

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -10,17 +10,18 @@ public class Movement : MonoBehaviour
     private Vector2 _movementDirection;
 
     [Range(1.0f, 10.0f)]
-    public float movementSpeed;
+    public float baseMovementSpeed;
 
     private Vector2 _lastDir;
-    private Rigidbody2D rb2D;
-    private Animator movementAnimator;
+    private Rigidbody2D _rb2D;
+    private Animator _movementAnimator;
+    private float _movementSpeed = 0;
     private bool _isMoving = true;
 
     private void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
-        movementAnimator = GetComponent<Animator>();
+        _rb2D = GetComponent<Rigidbody2D>();
+        _movementAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -36,7 +37,8 @@ public class Movement : MonoBehaviour
 
     public void ProcessInputs()
     {
-        _movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"),
+                                         Input.GetAxisRaw("Vertical"));        
 
         if (_movementDirection.x != 0 || _movementDirection.y != 0)
         {
@@ -45,27 +47,35 @@ public class Movement : MonoBehaviour
         }
 
         ProccesAnimation();
+
     }
 
     public void MovePlayer()
     {
-        rb2D.MovePosition(rb2D.position + _movementDirection * 
-                          movementSpeed * Time.fixedDeltaTime);
+        _rb2D.MovePosition(_rb2D.position + _movementDirection * 
+                          baseMovementSpeed * Time.fixedDeltaTime);
+
     }
 
     public void ProccesAnimation()
     {
-        movementAnimator.SetFloat("Horizontal", _lastDir.x);
-        movementAnimator.SetFloat("Vertical", _lastDir.y);
+        _movementAnimator.SetFloat("Horizontal", _lastDir.x);
+        _movementAnimator.SetFloat("Vertical", _lastDir.y);
+        _movementAnimator.SetFloat("Speed",
+                                   Mathf.Clamp(_movementDirection.magnitude, 0.0f, 1.0f));
+
     }
 
     public void DisableMovement()
     {
         _isMoving = false;
+
     }
 
     public void StartMoving()
     {
         _isMoving = true;
+
     }
+
 }
