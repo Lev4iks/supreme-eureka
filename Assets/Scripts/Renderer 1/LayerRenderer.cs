25ee69407461d9c786585efbd33bfa92b1fc8937
batovski <56854166+batovski@users.noreply.github.com>
2020-10-03 22:18:58 -0700

    Office Template
    Layer renderer + Office objects

---
+++ Assets/Scripts/Renderer 1/LayerRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class LayerRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private int baseLayer = 5000;
+
+    [SerializeField]
+    private float offSet = 0f;
+
+    [SerializeField]
+    private bool runOnlyOnce = true;
+
+    private Renderer myRenderer;
+
+
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        InvokeRepeating("RenderObject", 0.1f, 0.3f);
+    }
+
+    private void RenderObject()
+    {
+        myRenderer.sortingOrder = (int)(baseLayer - transform.position.y * 10 - offSet);
+        if (runOnlyOnce)
+            Destroy(this);
+    }
+}
